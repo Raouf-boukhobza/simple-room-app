030fd39192b2af58841860d53b2efb41423aacf5
raouf <raoufboukhobza3@gmail.com>
2024-08-03 22:57:19 +0100

    add the di

---
+++ app/src/main/java/com/raouf/simpleroomapp/data/local/ContactDB.kt
@@ -0,0 +1,15 @@
+package com.raouf.simpleroomapp.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+
+@Database(
+    entities = [Contanct::class],
+    version = 1
+)
+
+abstract class ContactDB : RoomDatabase() {
+    abstract val dao : ContectsDao
+    
+}
---
+++ app/src/main/java/com/raouf/simpleroomapp/di/DbModule.kt
@@ -0,0 +1,29 @@
+package com.raouf.simpleroomapp.di
+
+import android.content.Context
+import androidx.room.Room
+import com.raouf.simpleroomapp.data.local.ContactDB
+import com.raouf.simpleroomapp.data.local.ContectsDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DbModule {
+
+    @Provides
+    fun providedb(@ApplicationContext context : Context) : ContactDB{
+        return Room.databaseBuilder(context , ContactDB::class.java , "Contact")
+            .build()
+    }
+
+    @Provides
+    fun providedao(Conctactdb : ContactDB) : ContectsDao{
+        return Conctactdb.dao
+    }
+
+}
